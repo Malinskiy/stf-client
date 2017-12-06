5305c6c3134c8ead8ee34ba301d49ac3e21a2438
Akhmad Fathonih <akhmad.fathonih@P32136.local>
2017-12-06 12:38:42 +0900

    merge

--- lib/stf/client.rb
+++ lib/stf/client.rb
@@ -17,52 +17,57 @@ module Stf
 
     def get_devices
       response = execute '/api/v1/devices', Net::HTTP::Get
-      return response.devices
+      response.devices
     end
 
     def get_device(serial)
       response = execute "/api/v1/devices/#{serial}", Net::HTTP::Get
-      return response.device
+      response.device
     end
 
     def get_user
       response = execute '/api/v1/user', Net::HTTP::Get
-      return response.user
+      response.user
     end
 
     def get_user_devices
       response = execute '/api/v1/user/devices', Net::HTTP::Get
-      return response.devices
+      response.devices
     end
 
     def add_device(serial)
-      response = execute '/api/v1/user/devices', Net::HTTP::Post, {serial: serial}.to_json
-      return response.success
+      response = execute '/api/v1/user/devices', Net::HTTP::Post, { serial: serial }.to_json
+      response.success
+    end
+
+    def add_adb_public_key(id_pub)
+      response = execute '/api/v1/user/adbPublicKeys', Net::HTTP::Post, { adb: { publickey: id_pub } }.to_json
+      response.success
     end
 
     def remove_device(serial)
       response = execute "/api/v1/user/devices/#{serial}", Net::HTTP::Delete
-      return response.success
+      response.success
     end
 
     def start_debug(serial)
-      response = execute "/api/v1//user/devices/#{serial}/remoteConnect", Net::HTTP::Post
-      return response
+      response = execute "/api/v1/user/devices/#{serial}/remoteConnect", Net::HTTP::Post
+      response
     end
 
     def stop_debug(serial)
-      response = execute "/api/v1//user/devices/#{serial}/remoteConnect", Net::HTTP::Delete
-      return response.success
+      response = execute "/api/v1/user/devices/#{serial}/remoteConnect", Net::HTTP::Delete
+      response.success
     end
 
     private
 
-    def execute(relative_url, type, body='')
-      return execute_absolute @base_url + relative_url, type, body
+    def execute(relative_url, type, body = '')
+      execute_absolute @base_url + relative_url, type, body
     end
 
-    def execute_absolute(url, type, body='', limit = 10)
-      raise ArgumentError, 'too many HTTP redirects' if limit == 0
+    def execute_absolute(url, type, body = '', limit = 10)
+      raise ArgumentError, 'too many HTTP redirects' if limit.zero?
 
       uri          = URI.parse(url)
       http         = Net::HTTP.new(uri.host, uri.port)
@@ -72,19 +77,19 @@ module Stf
       response     = http.request(request)
 
       case response
-        when Net::HTTPSuccess then
-          json = JSON.parse(response.body, object_class: OpenStruct)
+      when Net::HTTPSuccess then
+        json = JSON.parse(response.body, object_class: OpenStruct)
 
-          logger.debug "API returned #{json}"
-        when Net::HTTPRedirection then
-          location = response['location']
-          logger.debug "redirected to #{location}"
-          return execute_absolute(location, type, body, limit - 1)
-        else
-          logger.error "API returned #{response.value}"
+        logger.debug "API returned #{json}"
+      when Net::HTTPRedirection then
+        location = response['location']
+        logger.debug "redirected to #{location}"
+        return execute_absolute(location, type, body, limit - 1)
+      else
+        logger.error "API returned #{response.value}"
       end
 
-      return json
+      json
     end
   end
 end
--- stf-client.gemspec
+++ stf-client.gemspec
@@ -4,12 +4,12 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'stf/version'
 
 Gem::Specification.new do |spec|
-  spec.name     = 'stf-client'
+  spec.name     = 'stf-client-neofreko'
   spec.version  = Stf::VERSION
-  spec.authors  = ['Anton Malinskiy']
-  spec.email    = ['anton@malinskiy.com']
-  spec.summary  = %q{Request devices from Smartphone Test Farm for adb connection}
-  spec.homepage = 'https://github.com/Malinskiy/stf-client'
+  spec.authors  = ['Akhmad Fathonih']
+  spec.email    = ['akhmadf@fathonih.com']
+  spec.summary  = %q{Request devices from Smartphone Test Farm for adb connection - forked from stf-client gem}
+  spec.homepage = 'https://github.com/neofreko/stf-client'
   spec.license  = 'MIT'
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.require_paths = ['lib']
