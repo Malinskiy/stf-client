7c44b4ff827f64027c111db303a7e1a0af71ee4d
Fred Cox <mcfedr@gmail.com>
2019-08-16 21:15:02 +0300

    Add command to import adb public key

--- lib/di.rb
+++ lib/di.rb
@@ -9,6 +9,7 @@ require 'stf/interactor/stop_all_debug_sessions_interactor'
 require 'stf/interactor/remove_all_user_devices_interactor'
 require 'stf/interactor/get_keys_interactor'
 require 'stf/interactor/get_values_interactor'
+require 'stf/interactor/add_adb_public_key'
 require 'stf/validate/uri_validator'
 
 class DI
@@ -63,6 +64,10 @@ class DI
 
       c.register(:uri_validator,
                  -> {Stf::URIValidator.new},
+                 memoize: true)
+
+      c.register(:add_adb_public_key_interactor,
+                 -> {Stf::AddAdbPublicKeyInteractor.new},
                  memoize: true)
     end
 
